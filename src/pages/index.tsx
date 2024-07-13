6159fb0ee242b2067e27136644ab694cb0432cfb
haycarlitos <carlos.castilloperera@gmail.com>
2024-07-13 01:23:55 -0600

    branding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,8 +44,10 @@ export default function Home() {
 	return (
 		<div>
 			<div className="flex flex-col items-center justify-center align-middle h-screen">
-				<p className="text-2xl mb-5">We are what we repeatedly do. Excellence, then, is not an act, but a habit.
-				</p>
+			<p className="text-2xl mb-5">We are what we repeatedly do. Excellence, then, is not an act, but a habit.
+			</p>
+			<p className="text-2xl mb-5">Power Habits
+			</p>
 				<IDKitWidget
 					action={process.env.NEXT_PUBLIC_WLD_ACTION!}
 					app_id={process.env.NEXT_PUBLIC_WLD_APP_ID as `app_${string}`}
